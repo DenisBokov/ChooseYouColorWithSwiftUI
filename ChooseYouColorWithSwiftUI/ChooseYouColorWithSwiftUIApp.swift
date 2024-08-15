9a4d771dd2a1ce139593ba16128013db407a94f7
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-08-15 18:20:04 +0300

    Initial Commit

---
+++ ChooseYouColorWithSwiftUI/ChooseYouColorWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYouColorWithSwiftUIApp.swift
+//  ChooseYouColorWithSwiftUI
+//
+//  Created by Denis Bokov on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYouColorWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChooseYouColorWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChooseYouColorWithSwiftUI
+//
+//  Created by Denis Bokov on 15.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
